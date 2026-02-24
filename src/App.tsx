f1bf4c820c866741d65060016ebdab5cf38b6380
Leonardo <leofronchi@outlook.com>
2026-02-23 21:44:59 -0300

    Criando homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Homepage } from './pages';
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
-      <div className="bg-white p-10 rounded-2xl shadow-2xl text-center space-y-4">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Tailwind está funcionando 🚀
-        </h1>
-        <p className="text-gray-600">
-          Se você está vendo fundo escuro e card branco arredondado,
-          está tudo certo.
-        </p>
-        <button className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-          Botão teste
-        </button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,10 @@
+
+const Homepage = () => {
+    return (
+        <h1>
+            Olá
+        </h1>
+    )
+}
+
+export default Homepage
